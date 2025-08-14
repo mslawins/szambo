960dc8e7c2b063753acf1cd623b4e7d764490f08
Marek Sławiński <marek.slawinski@live.com>
2025-08-14 15:29:18 +0200

    Add unit tests

--- src/json/paths.rs
+++ src/json/paths.rs
@@ -29,3 +29,43 @@ fn collect_paths(value: &Value, prefix: String, paths: &mut Vec<String>) -> Resu
         )),
     }
 }
+#[cfg(test)]
+mod get_json_paths {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn should_return_no_paths_for_empty_json_file() {
+        let mut data = json!({});
+        let expected: Vec<String> = vec![];
+
+        let result = get_json_paths(&mut data);
+
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn should_return_all_paths() {
+        let mut data = json!({
+            "foo": "value",
+            "bar": {
+                "baz": "value",
+            },
+            "aaa": {},
+            "bbb": {
+                "ccc": {
+                    "ddd": "value"
+                }
+            }
+        });
+        let expected: Vec<String> = vec![
+            "bar.baz".to_owned(),
+            "bbb.ccc.ddd".to_owned(),
+            "foo".to_owned(),
+        ];
+
+        let result = get_json_paths(&mut data);
+
+        assert_eq!(result.unwrap(), expected);
+    }
+}
